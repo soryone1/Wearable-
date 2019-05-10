ef1a0a1e3a406a46fb8c3a7fd88fff4be459b74d
Rui Wang <43215538+soryone1@users.noreply.github.com>
2019-05-09 20:16:33 -0400

    add the first sketch 5.10
    worked with 2 acc

---
+++ tca9548a_lsm303_test.ino
@@ -0,0 +1,111 @@
+/**
+   TCA9548 I2CScanner.pde -- I2C bus scanner for Arduino
+
+   Based on code c. 2009, Tod E. Kurt, http://todbot.com/blog/
+
+*/
+
+#include <Wire.h>
+
+#define TCAADDR 0x70
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+
+Adafruit_LSM303_Accel_Unified accel2 = Adafruit_LSM303_Accel_Unified(12345);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+
+
+// standard Arduino setup()
+void setup()
+{
+  while (!Serial);
+  delay(1000);
+
+  Wire.begin();
+
+
+  tcaselect(2);
+  /* Initialise the sensor */
+  if (!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while (1);
+  }
+
+  tcaselect(4);
+  /* Initialise the sensor */
+  if (!accel2.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while (1);
+  }
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+}
+
+void loop()
+{
+  tcaselect(2);
+  /* Get a new sensor event */
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  Serial.print("2:");
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  "); Serial.print("m/s^2 ");
+
+  /* Note: You can also get the raw (non unified values) for */
+  /* the last data sample as follows. The .getEvent call populates */
+  /* the raw values used below. */
+  //Serial.print("X Raw: "); Serial.print(accel.raw.x); Serial.print("  ");
+  //Serial.print("Y Raw: "); Serial.print(accel.raw.y); Serial.print("  ");
+  //Serial.print("Z Raw: "); Serial.print(accel.raw.z); Serial.println("");
+  tcaselect(4);
+  /* Get a new sensor event */
+  //sensors_event_t event;
+  accel2.getEvent(&event);
+
+  Serial.print("4:");
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  "); Serial.println("m/s^2 ");
+
+
+
+  /* Delay before the next sample */
+  delay(500);
+}
